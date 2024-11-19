35b33d5152220c8ec741598fa6c201561a163785
Sadia Khan <233544@students.au.edu.pk>
2024-11-19 20:07:24 +0500

    Add files via upload

---
+++ Lab07/SqApp/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Forms;
+
+namespace SquareCalculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button buttonCalculate;
+        private ListBox listBoxSquares;
+
+        // Dispose method to clean up resources
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Method to initialize components (UI controls)
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.listBoxSquares = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(12, 12);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(260, 40);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Calculate Squares";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+
+            // 
+            // listBoxSquares
+            // 
+            this.listBoxSquares.FormattingEnabled = true;
+            this.listBoxSquares.Location = new System.Drawing.Point(12, 58);
+            this.listBoxSquares.Name = "listBoxSquares";
+            this.listBoxSquares.Size = new System.Drawing.Size(260, 199);
+            this.listBoxSquares.TabIndex = 1;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 281);
+            this.Controls.Add(this.listBoxSquares);
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "Form1";
+            this.Text = "Square Calculator";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Lab07/SqApp/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SquareCalculator
+{
+    public partial class Form1 : Form
+    {
+        // Constructor to initialize the form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method to calculate the square of a given number
+        private int CalculateSquare(int number)
+        {
+            return number * number;
+        }
+
+        // Event handler for the button click
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            listBoxSquares.Items.Clear(); // Clear any previous results
+
+            // Loop through the first 10 numbers (1 to 10)
+            for (int i = 1; i i <= 10; i++)
+            {
+                // Call CalculateSquare method to calculate the square
+                int square = CalculateSquare(i);
+
+                // Display the result in the ListBox
+                listBoxSquares.Items.Add($"Square of {i} is {square}");
+            }
+        }
+    }
+}
