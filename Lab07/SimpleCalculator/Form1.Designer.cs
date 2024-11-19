578e94d7b9aee1bb6f4448b22bcd268dfbdac903
Sadia Khan <233544@students.au.edu.pk>
2024-11-19 19:54:44 +0500

    Add files via upload

---
+++ Lab07/SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.cmbOperation = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(22, 24);
+            this.txtNum1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(76, 20);
+            this.txtNum1.TabIndex = 0;
+            this.txtNum1.TextChanged += new System.EventHandler(this.txtNum1_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(188, 24);
+            this.txtNum2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(76, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // cmbOperation
+            // 
+            this.cmbOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperation.FormattingEnabled = true;
+            this.cmbOperation.Items.AddRange(new object[] {
+            "Add",
+            "Subtract",
+            "Multiply",
+            "Divide"});
+            this.cmbOperation.Location = new System.Drawing.Point(105, 24);
+            this.cmbOperation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.Size = new System.Drawing.Size(76, 21);
+            this.cmbOperation.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(22, 57);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(240, 24);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(22, 98);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(43, 13);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 162);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cmbOperation);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Calculator App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.ComboBox cmbOperation;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ Lab07/SimpleCalculator/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(txtNum1.Text);
+                double num2 = Convert.ToDouble(txtNum2.Text);
+                string operation = cmbOperation.SelectedItem.ToString();
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "Add":
+                        result = Add(num1, num2);
+                        break;
+                    case "Subtract":
+                        result = Subtract(num1, num2);
+                        break;
+                    case "Multiply":
+                        result = Multiply(num1, num2);
+                        break;
+                    case "Divide":
+                        if (num2 == 0)
+                        {
+                            MessageBox.Show("Cannot divide by zero.");
+                            return;
+                        }
+                        result = Divide(num1, num2);
+                        break;
+                }
+
+                lblResult.Text = "Result: " + result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private double Add(double a, double b) => a + b;
+
+        private double Subtract(double a, double b) => a - b;
+
+        private double Multiply(double a, double b) => a * b;
+
+        private double Divide(double a, double b) => a / b;
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
