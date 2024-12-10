da16cc41a45aa4c27cb8a2bcd9465c256ea47803
Sadia Khan <233544@students.au.edu.pk>
2024-12-10 20:26:45 +0500

    Add files via upload

---
+++ Lab11/Task1/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for BringBackPocoyo.xaml
+    /// </summary>
+    public partial class BringBackPocoyo : Window
+    {
+        public BringBackPocoyo()
+        {
+            InitializeComponent();
+        }
+    }
+}
