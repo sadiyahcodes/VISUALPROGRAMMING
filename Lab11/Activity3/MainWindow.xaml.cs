27077c7c68b4ab0ec0819698aab39aed4cb57c5f
Sadia Khan <233544@students.au.edu.pk>
2024-12-10 14:47:24 +0500

    Add files via upload

---
+++ Lab11/Activity3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFComboBoxControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for the first ComboBox
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = sender as ComboBox;
+
+            if (combo != null && combo.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox1.Text = selectedItem.Content.ToString();
+            }
+        }
+
+        // Event handler for the second ComboBox
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = sender as ComboBox;
+
+            if (combo != null && combo.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox2.Text = selectedItem.Content.ToString();
+            }
+        }
+    }
+}
