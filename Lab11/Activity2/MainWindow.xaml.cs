9ac5e79d3017e61ac5335d7b91ef03075a60f359
Sadia Khan <233544@students.au.edu.pk>
2024-12-10 14:16:47 +0500

    Add files via upload

---
+++ Lab11/Activity2/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFCheckBoxControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") textBox1.Text = "2 state CheckBox is checked."; else textBox2.Text = "3 state CheckBox is checked.";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+
+            if (cb.Name == "checkBox1") textBox1.Text = "2 state CheckBox is unchecked."; else textBox2.Text = "3 state CheckBox is unchecked.";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            textBox2.Text = "3 state CheckBox is in indeterminate state.";
+        }
+
+    }
+}
